a3901b40c75bc8f8e37ba8c71c4bcec4d22e04cc
mohammeddl <mohammed_daali2@um5.ac.ma>
2024-11-17 17:09:21 +0100

    first commit

---
+++ src/test/java/com/citronix/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citronix.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
