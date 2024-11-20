87aca8fb462490098fa92418d37fe259467eb27f
mohammeddl <mohammed_daali2@um5.ac.ma>
2024-11-20 15:11:57 +0100

    define SaleDTO and Sale model with validation

--- src/main/java/com/citronix/demo/dto/SaleDTO.java
+++ src/main/java/com/citronix/demo/dto/SaleDTO.java
@@ -1,5 +1,18 @@
 package com.citronix.demo.dto;
 
-public class SaleDTO {
-    
+import java.time.LocalDate;
+
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+public record SaleDTO(
+
+        Long id,
+        @Positive(message = "Unit price must be greater than 0") double unitPrice,
+        @Positive(message = "Quantity must be greater than 0") double quantity,
+        double revenue,
+        @NotNull(message = "Date is required") @FutureOrPresent(message = "Date cannot be in the past") LocalDate date,
+        @NotBlank(message = "Client name is required") String client) {
 }
